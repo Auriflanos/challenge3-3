a9dc967d76103eb072f167a7d37b7956996b5461
Lucas Gonçalves <66454089+Auriflanos@users.noreply.github.com>
2022-02-10 15:58:04 +0100

    Error fix and final solution

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,6 @@
 const modalOverlay = document.querySelector('.modal-overlay')/*document.querySelector vai selecionar o a tag .modal-overlay */
 const cards = document.querySelectorAll('.card')  /* aqui ele seleciona um grupo no caso sera o a Tag card */
-const course = document.querySelectorAll('.course')
+const course = document.querySelector('.course')
 
 for (let card of cards) {
     card.addEventListener("click", function () {
@@ -14,6 +14,7 @@ course.addEventListener("click", function () {
     console.log('course clicked', course);
     const blogId = course.getAttribute("id")
     window.location.href = `https://blog.rocketseat.com.br/${blogId}`
+    console.log('course clicked', blogId);
 })
 
 
